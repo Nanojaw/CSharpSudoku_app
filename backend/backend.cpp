62615e6aaa97ce69702bfd8ba509a2c98bcf8638
XpulseIV <malteaxner363@gmail.com>
2022-04-02 11:11:29 +0200

    First commit

---
+++ backend/backend.cpp
@@ -0,0 +1,35 @@
+#include "iostream"
+
+#include "backend.h"
+
+const char* GenerateSudoku()
+{
+	return nullptr;
+}
+
+const char* GenerateHexSudoku()
+{
+    return nullptr;
+}
+
+void Free(sudokutype type)
+{
+}
+
+const char* LoadSudoku()
+{
+    return nullptr;
+}
+
+const char* LoadHexSudoku()
+{
+    return nullptr;
+}
+
+void SaveSudoku()
+{
+}
+
+void SaveHexSudoku()
+{
+}
---
+++ tester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tester {
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var lel = backend.GenerateSudoku();
+
+            Console.WriteLine(lel);
+        }
+    }
+}
