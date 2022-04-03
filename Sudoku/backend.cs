ee50704191089eca5a3f76d199f495a5b1294609
XpulseIV <malteaxner363@gmail.com>
2022-04-03 10:10:54 +0200

    made correct function

--- Sudoku/backend.cs
+++ Sudoku/backend.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sudoku
 {
@@ -9,13 +11,50 @@ namespace Sudoku
     {
         /// <summary>
         /// Generates Sudoku;
+        /// Uses a variable toBeFilled to denote how many percent of the board the generator should fill
         /// </summary>
         /// <returns> List of ints </returns>
-        public static List<int> GenerateSudoku()
+        public static List<int> GenerateSudoku(double toBeFilled)
         {
+            var randomNumberGenerator = new Random();
+
+            var toBeRemoved = 1 - toBeFilled;
+            var numOfDeletions = Math.Floor(toBeRemoved * 81);
+
             var sudokuBoard = new SudokuBoard();
 
             sudokuBoard.Solver.SolveThePuzzle();
+
+            for (int i = 0; i < numOfDeletions; i++)
+            {
+                var cellIndex = randomNumberGenerator.Next(0, 80);
+
+                while (sudokuBoard.GetCell(cellIndex).Value == -1)
+                {
+                    cellIndex = randomNumberGenerator.Next(0, 80);
+                }
+
+                sudokuBoard.SetCellValue(-1, cellIndex);
+            }
+
+            while (!sudokuBoard.Solver.CheckTableStateIsValid())
+            {
+                sudokuBoard.Clear();
+
+                sudokuBoard.Solver.SolveThePuzzle();
+
+                for (int i = 0; i < numOfDeletions; i++)
+                {
+                    var cellIndex = randomNumberGenerator.Next(0, 80);
+
+                    while (sudokuBoard.GetCell(cellIndex).Value == -1)
+                    {
+                        cellIndex = randomNumberGenerator.Next(0, 80);
+                    }
+
+                    sudokuBoard.SetCellValue(-1, cellIndex);
+                }
+            }
 
             var sudokuPuzzle = new List<int>();
 
