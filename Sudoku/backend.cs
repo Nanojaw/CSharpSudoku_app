f72b7e93c59795006d007db05206644cc30aac40
TuffenDuffen <reg_i@wext.se>
2022-04-03 11:54:04 +0200

    animations

--- Sudoku/backend.cs
+++ Sudoku/backend.cs
@@ -8,6 +8,20 @@ namespace Sudoku
     /// </summary>
     public static class Backend
     {
+        private static readonly Dictionary<int, int> ToSquare = new Dictionary<int, int>(){
+            {0, 0}, {1, 0}, {2, 0}, {3, 3}, {4, 3}, {5, 3}, {6, 6}, {7, 6}, {8, 6},
+            {9, 0}, {10, 0}, {11, 0}, {12, 3}, {13, 3}, {14, 3}, {15, 6}, {16, 6}, {17, 6},
+            {18, 0}, {19, 0}, {20, 0}, {21, 3}, {22, 3}, {23, 3}, {24, 6}, {25, 6}, {26, 6},
+
+            {27, 27}, {28, 27}, {29, 27}, {30, 30}, {31, 30}, {32, 30}, {33, 33}, {34, 33}, {35, 33},
+            {36, 27}, {37, 27}, {38, 27}, {39, 30}, {40, 30}, {41, 30}, {42, 33}, {43, 33}, {44, 33},
+            {45, 27}, {46, 27}, {47, 27}, {48, 30}, {49, 30}, {50, 30}, {51, 33}, {52, 33}, {53, 33},
+
+            {54, 54}, {55, 54}, {56, 54}, {57, 57}, {58, 57}, {59, 57}, {60, 60}, {61, 60}, {62, 60},
+            {63, 54}, {64, 54}, {65, 54}, {66, 57}, {67, 57}, {68, 57}, {69, 60}, {70, 60}, {71, 60},
+            {72, 54}, {73, 54}, {74, 54}, {75, 57}, {76, 57}, {77, 57}, {78, 60}, {79, 60}, {80, 60},
+        };
+
         /// <summary>
         /// Generates Sudoku;
         /// </summary>
@@ -23,6 +37,38 @@ namespace Sudoku
             sudokuBoard.Cells.ForEach(cell => sudokuPuzzle.Add(cell.Value));
 
             return sudokuPuzzle;
+        }
+
+        /// <summary>
+        /// Checks if ou can insert the number at the specifiedindex
+        /// </summary>
+        /// <param name="sudoku">The sudoku board</param>
+        /// <param name="number">The number to fit</param>
+        /// <param name="index"> The index of the number</param>
+        /// <returns></returns>
+        public static bool Fits(List<int> sudoku, int number, int index)
+        {
+            // Horizontal check
+            for (var i = 0; i < 9; i++)
+            {
+                if (sudoku[i + (index / 9) * 9] == number) return false;
+            }
+
+            for (var i = 0; i < 9; i++)
+            {
+                if (sudoku[i * 9 + (index % 9)] == number) return false;
+            }
+
+            var square = ToSquare[index];
+            for (var i = 0; i < 27; i += 9)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    if (sudoku[square + i + j] == number) return false;
+                }
+            }
+
+            return true;
         }
     }
 }
--- frontend/MainWindow.xaml.cs
+++ frontend/MainWindow.xaml.cs
@@ -9,9 +9,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using Sudoku;
 
 namespace frontend
 {
@@ -20,9 +23,65 @@ namespace frontend
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<int> _sudoku;
+
         public MainWindow()
         {
             InitializeComponent();
+            _sudoku = Backend.GenerateSudoku().GetRange(0, 81);
+            FillGrid();
+        }
+
+        private void CellClick(object sender, RoutedEventArgs e)
+        {
+            ((Button) sender).KeyUp += SetNumber;
+        }
+
+        private void SetNumber(object sender, KeyEventArgs e)
+        {
+            if (e.Key is not (>= Key.D1 and <= Key.D9 or >= Key.NumPad1 and <= Key.NumPad9 or Key.Back)) return;
+
+            var cell = (Button) sender;
+
+            var key = new KeyConverter().ConvertToString(e.Key) ?? "";
+
+            // Removing a number
+            if (e.Key == Key.Back)
+            {
+                cell.Content = "";
+                _sudoku[MainBoard.Children.IndexOf(cell)] = -1;
+                cell.KeyUp -= SetNumber;
+                return;
+            }
+
+            // Only insert number if it fits
+            if (Backend.Fits(_sudoku, int.Parse(key), MainBoard.Children.IndexOf(cell)))
+            {
+                cell.Content = key;
+                _sudoku[MainBoard.Children.IndexOf(cell)] = int.Parse(key);
+                cell.KeyUp -= SetNumber;
+            }
+            else // Otherwise play an animation
+            {
+                cell.Background = new SolidColorBrush(((SolidColorBrush)cell.Background).Color);
+                var anim = new ColorAnimation()
+                {
+                    From = ((SolidColorBrush)cell.Background).Color,
+                    To = Colors.Red,
+                    Duration = new Duration(TimeSpan.FromSeconds(0.2)),
+                    AutoReverse = true
+                };
+                cell.Background.BeginAnimation(SolidColorBrush.ColorProperty, anim);
+            }
+        }
+
+        private void FillGrid()
+        {
+            for (var i = 0; i < MainBoard.Children.Count; i++)
+            {
+                var cell = (Button) MainBoard.Children[i];
+                cell.Content = _sudoku[i].ToString();
+            }
         }
     }
 }
